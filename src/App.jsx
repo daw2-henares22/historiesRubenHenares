bacfce8a15a794bcce9de6f621d3481da41fdd73
daw2-henares22 <henareshidalgoruben@fpllefia.com>
2024-05-15 16:52:54 +0200

    nextUi

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Camera, Icon, icons } from 'lucide-react'
+// import { color } from 'framer-motion'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+  const LucideIcon = icons[name];
+
+  return (
+    <div className="flex flex-wrap gap-4 items-center">
+      <Button color="primary" variant="solid">
+        Solid
+      </Button>
+      <Button color="primary" variant="faded">
+        Faded
+      </Button>
+      <Button color="primary" variant="bordered">
+        Bordered
+      </Button>
+      <Button color="primary" variant="light">
+        Light
+      </Button>
+      <Button color="primary" variant="flat">
+        Flat
+      </Button>
+      <Button color="primary" variant="ghost">
+        Ghost
+      </Button>
+      <Button color="primary" variant="shadow">
+        Shadow
+      </Button>
+      <Camera color="red" size={48} />;
+      <Camera size={48} fill="red" />;
+      <Icon iconNode={burger} />
+      <LucideIcon color={color} size={size} />;
+      <Icon name="Home" />;
+    </div>
+  );
+}
